28df2dab46e867b4dcbb89c99081e45730f3da49
venkatanarayana siliveri <svn99509@gmail.com>
2025-06-06 17:52:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def user_input():
     input_data = {}
     features = ['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10',
                 'V11', 'V12', 'V13', 'V14', 'V16', 'V17', 'V18', 'V19', 'V20', 'V21',
-                'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'Time', 'Amount']
+                'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'Scaled_Time', 'Scaled_Amount']
 
     for i, feature in enumerate(features):
         default_val = 0.0 if feature.startswith("V") else 0.5
